df3546ef8c525cbee1a4c2a925166e3f3f5450c0
Sucharek233 <jansousek11@gmail.com>
2026-04-20 22:27:37 +0200

    Zmena logiky postavicky

--- sibenice/Postavicka.cs
+++ sibenice/Postavicka.cs
@@ -1,4 +1,6 @@
-﻿namespace sibenice {
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace sibenice {
     internal class Postavicka {
         public Postavicka() {
             InitParts();
@@ -6,30 +8,27 @@
 
         const int postavickaSize = 200;
 
-        private class LinePart {
-            public Point Start { get; set; }
-            public Point End { get; set; }
+        struct Part {
+            public char Type;
+            public Point A;
+            public Point B;
+            public int Radius;
         }
 
-        private class CirclePart {
-            public Point Center { get; set; }
-            public int Radius { get; set; }
-        }
-
-        private List<object> parts;
+        private List<Part> parts;
 
         private void InitParts() {
-            parts = new List<object> {
-                new LinePart   { Start =  new Point(10, 180),  End = new Point(100, 180) }, // základna
-                new LinePart   { Start =  new Point(55, 180),  End = new Point(55, 20)   }, // sloup
-                new LinePart   { Start =  new Point(55, 20),   End = new Point(130, 20)  }, // trám
-                new LinePart   { Start =  new Point(130, 20),  End = new Point(130, 50)  }, // provaz
-                new CirclePart { Center = new Point(130, 65),  Radius = 15 },               // hlava
-                new LinePart   { Start =  new Point(130, 80),  End = new Point(130, 130) }, // tělo
-                new LinePart   { Start =  new Point(130, 90),  End = new Point(110, 110) }, // levá ruka
-                new LinePart   { Start =  new Point(130, 90),  End = new Point(150, 110) }, // pravá ruka
-                new LinePart   { Start =  new Point(130, 130), End = new Point(110, 160) }, // levá noha
-                new LinePart   { Start =  new Point(130, 130), End = new Point(150, 160) }  // pravá noha
+            parts = new List<Part> {
+                new Part { Type = 'L', A = new Point(10, 180),  B = new Point(100,180) }, // základna
+                new Part { Type = 'L', A = new Point(55, 180),  B = new Point(55,20 )  }, // sloup
+                new Part { Type = 'L', A = new Point(55, 20),   B = new Point(130,20)  }, // trám
+                new Part { Type = 'L', A = new Point(130, 20),  B = new Point(130,50)  }, // provaz
+                new Part { Type = 'C', A = new Point(130, 65),  Radius = 15            }, // hlava
+                new Part { Type = 'L', A = new Point(130, 80),  B = new Point(130,130) }, // tělo
+                new Part { Type = 'L', A = new Point(130, 90),  B = new Point(110,110) }, // levá ruka
+                new Part { Type = 'L', A = new Point(130, 90),  B = new Point(150,110) }, // pravá ruka
+                new Part { Type = 'L', A = new Point(130, 130), B = new Point(110,160) }, // levá noha
+                new Part { Type = 'L', A = new Point(130, 130), B = new Point(150,160) }  // pravá noha
             };
         }
 
@@ -39,21 +38,22 @@
             Point posunutiNaStred = new Point((velikost.X - postavickaSize) / 2, (velikost.Y - postavickaSize) / 2);
 
             for (int i = 0; i < pokusy && i < parts.Count; i++) {
-                if (parts[i] is LinePart lp) {
+                Part p = parts[i];
+                if (p.Type == 'L') {
                     g.DrawLine(
                         Pens.Black,
-                        lp.Start.X + posunutiNaStred.X,
-                        lp.Start.Y + posunutiNaStred.Y,
-                        lp.End.X   + posunutiNaStred.X,
-                        lp.End.Y   + posunutiNaStred.Y
+                        p.A.X + posunutiNaStred.X,
+                        p.A.Y + posunutiNaStred.Y,
+                        p.B.X + posunutiNaStred.X,
+                        p.B.Y + posunutiNaStred.Y
                     );
-                } else if (parts[i] is CirclePart cp) {
+                } else {
                     g.DrawEllipse(
                         Pens.Black,
-                        cp.Center.X - cp.Radius + posunutiNaStred.X,
-                        cp.Center.Y - cp.Radius + posunutiNaStred.Y,
-                        cp.Radius * 2,
-                        cp.Radius * 2
+                        p.A.X - p.Radius + posunutiNaStred.X,
+                        p.A.Y - p.Radius + posunutiNaStred.Y,
+                        p.Radius * 2,
+                        p.Radius * 2
                     );
                 }
             }
